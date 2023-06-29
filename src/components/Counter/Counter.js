4a82f2335a22af5d6aa61604a7c7fba66e2e112e
Helder Berto <helder.burato@gmail.com>
2023-06-29 13:56:44 +0000

    Update 2 files

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,23 +1,29 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
-function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+import Spinner from "../Spinner";
+
+const useCount = () => {
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
+    const savedValue = window.localStorage.getItem("saved-count");
+    setCount(savedValue ? Number(savedValue) : 0);
+  }, []);
+
+  React.useEffect(() => {
+    window.localStorage.setItem("saved-count", count);
   }, [count]);
 
+  return [count, setCount];
+};
+
+function Counter() {
+  const [count, setCount] = useCount();
+
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
-      Count: {count}
+    <button className="count-btn" onClick={() => setCount(count + 1)}>
+      Count: {typeof count === "number" ? count : <Spinner />}
     </button>
   );
 }
